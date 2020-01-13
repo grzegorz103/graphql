3dfcac83f9e13d710d63afe331a262e8d38614ea
grzegorz103 <grzesiek555_12@outlook.com>
2020-01-13 21:09:55 +0100

    Add test files

--- src/test/java/com/system/car/queries/BrandQueryTests.java
+++ src/test/java/com/system/car/queries/BrandQueryTests.java
@@ -1,4 +1,43 @@
 package com.system.car.queries;
 
+import com.system.car.models.Brand;
+import com.system.car.services.BrandService;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BrandQueryTests {
+
+    @Mock
+    private BrandService brandService;
+
+    @InjectMocks
+    private BrandQuery brandQuery;
+
+    private List<Brand> brands;
+
+    @Before
+    public void setup(){
+        brands = Arrays.asList(mock(Brand.class), mock(Brand.class));
+        when(brandService.getAll()).thenReturn(brands);
+    }
+
+    @Test
+    public void getAllTest(){
+        assertThat(brandQuery.getBrands()).isEqualTo(brands);
+        assertThat(brandQuery.getBrands().size()).isEqualTo(brands.size());
+    }
+
 }
--- src/test/java/com/system/car/queries/CarQueryTests.java
+++ src/test/java/com/system/car/queries/CarQueryTests.java
@@ -1,4 +1,44 @@
 package com.system.car.queries;
 
+import com.system.car.models.Brand;
+import com.system.car.models.Car;
+import com.system.car.services.BrandService;
+import com.system.car.services.CarService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CarQueryTests {
+
+    @Mock
+    private CarService carService;
+
+    @InjectMocks
+    private CarQuery carQuery;
+
+    private List<Car> cars;
+
+    @Before
+    public void setup() {
+        cars = Arrays.asList(mock(Car.class), mock(Car.class));
+        when(carService.getCars()).thenReturn(cars);
+    }
+
+    @Test
+    public void getAllTest() {
+        assertThat(carQuery.getCars()).isEqualTo(cars);
+        assertThat(carQuery.getCars().size()).isEqualTo(cars.size());
+    }
+
 }
