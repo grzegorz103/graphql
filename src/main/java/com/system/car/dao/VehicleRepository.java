b89d7e316a5c088846b98a51c636d6be387f56da
grzegorz103 <grzesiek555_12@outlook.com>
2020-04-14 21:15:53 +0200

    Add get vehicles by brand id method

--- src/main/java/com/system/car/dao/VehicleRepository.java
+++ src/main/java/com/system/car/dao/VehicleRepository.java
@@ -1,6 +1,8 @@
 package com.system.car.dao;
 
 import com.system.car.models.Vehicle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -16,4 +18,10 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
             "v.year")
     List<Vehicle> findAllSorted();
 
+    @Query("SELECT v FROM Vehicle v " +
+            "WHERE v.brand.id = :brandId "+
+            "ORDER BY "+
+    "CASE WHEN v.brand IS NULL THEN 1 ELSE 0 END," +
+            "v.year")
+    Page<Vehicle> findAllByBrandId(Long brandId, Pageable pageable);
 }
