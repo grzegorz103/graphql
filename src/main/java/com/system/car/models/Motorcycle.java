da3b947dfdc894429e225f71b554bd84876b7f72
grzegorz103 <grzesiek555_12@outlook.com>
2020-05-16 14:23:33 +0200

    Configure db for test cases

--- src/main/java/com/system/car/models/Motorcycle.java
+++ src/main/java/com/system/car/models/Motorcycle.java
@@ -3,8 +3,13 @@ package com.system.car.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
 
 @Entity
 @Table(name = "motorcycles")
@@ -12,5 +17,8 @@ import javax.persistence.*;
 @Data
 public class Motorcycle extends Vehicle {
 
+    public Motorcycle(Long id, Brand brand, String model, Integer year, List<String> images, String info) {
+        super(id, brand, model, year, images, info);
+    }
 
 }
