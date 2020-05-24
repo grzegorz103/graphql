3927eb34c1a81cc2481885a3a74608f840d4b4c6
grzegorz103 <grzesiek555_12@outlook.com>
2020-05-24 20:11:52 +0200

    Add BrandControllerTest.java

--- src/test/java/com/system/car/api/rest/controllers/BrandControllerTest.java
+++ src/test/java/com/system/car/api/rest/controllers/BrandControllerTest.java
@@ -1,10 +1,10 @@
 package com.system.car.api.rest.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.system.car.api.rest.dto.in.CarIn;
+import com.system.car.api.rest.dto.in.BrandIn;
 import com.system.car.models.Brand;
 import com.system.car.models.Car;
-import com.system.car.services.abstr.CarService;
+import com.system.car.services.abstr.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -15,16 +15,17 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,63 +33,46 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-class CarControllerTest {
+public class BrandControllerTest {
+
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private CarService carService;
+    private BrandService brandService;
 
-    private final String carApiURL = "/api/v1/cars";
+    private final String brandApiURL = "/api/v1/brands";
 
     @Test
-    void getById() throws Exception {
-        when(carService.getCarById(anyLong())).thenReturn(mock(Car.class));
-        mockMvc.perform(get(carApiURL + "/1"))
+    void getByIdTest() throws Exception {
+        when(brandService.getById(anyLong()))
+                .thenReturn(mock(Brand.class));
+        mockMvc.perform(get(brandApiURL + "/1"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
     void getAll() throws Exception {
-        when(carService.getCarsPaged(any(Pageable.class)))
-                .thenReturn(new PageImpl<>(Arrays.asList(mock(Car.class), mock(Car.class))));
-        mockMvc.perform(get(carApiURL))
+        when(brandService.getAllPaged(any(Pageable.class)))
+                .thenReturn(new PageImpl<>(Arrays.asList(mock(Brand.class), mock(Brand.class))));
+        mockMvc.perform(get(brandApiURL))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
     void create() throws Exception {
-        CarIn car = new CarIn(1L,"test",2000,Collections.emptyList(),"test");
-        when(carService.create(anyString(), anyInt(), anyList(), anyString(), anyLong()))
-                .thenReturn(mock(Car.class));
-        mockMvc.perform(post(carApiURL)
-                .content(new ObjectMapper().writeValueAsString(car))
+        BrandIn brand = new BrandIn("test");
+        when(brandService.create(anyString()))
+                .thenReturn(mock(Brand.class));
+        mockMvc.perform(post(brandApiURL)
+                .content(new ObjectMapper().writeValueAsString(brand))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept("application/hal+json"))
                 .andDo(print())
                 .andExpect(status().isCreated());
     }
 
-    @Test
-    void update() throws Exception {
-        Car car = new Car(1L, new Brand(1L, null, "test"), "test", 1, Collections.emptyList(), "test");
-        when(carService.update(anyLong(), anyString(), anyInt(), anyLong()))
-                .thenReturn(mock(Car.class));
-        mockMvc.perform(put(carApiURL + "/1")
-                .content(new ObjectMapper().writeValueAsString(car))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept("application/hal+json"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void deleteById() throws Exception {
-        when(carService.delete(anyLong())).thenReturn(1L);
-        mockMvc.perform(delete(carApiURL + "/1"))
-                .andExpect(status().isOk());
-    }
 }
